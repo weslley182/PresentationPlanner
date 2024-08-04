385c7597e38cff2195b7e9143e500dc8ec0edbdd
Wesley Machado <wesley.machado@tooseguros.com.br>
2024-08-04 17:01:02 -0300

    Add project files.

---
+++ PresentationPlanner/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PresentationPlanner.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        _logger.LogWarning("Acessando mettodo get.");
+    }
+}
