3edefe17f3fe0d90680a60e3f4fba0e0c8cdde57
Wesley Machado <wesley.machado@tooseguros.com.br>
2024-08-05 11:05:01 -0300

    update post

--- PresentationPlanner/Areas/Contacts/Models/Contact.cs
+++ PresentationPlanner/Areas/Contacts/Models/Contact.cs
@@ -1,13 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PresentationPlanner.Areas.Contacts.Models;
 
 public class Contact
 {
     public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    [MinLength(3)]
     public string? Name { get; set; }
+
+    [Required]
+    [RegularExpression(@"^[1-9][0-9]{9}$", ErrorMessage = "Invalid phone number.")]
     public string? Phone { get; set; }
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid Email.")]
     public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
     public DateTime Birth { get; set; }
     public string? PhotoUrl { get; set; }
 
--- PresentationPlanner/Areas/Contacts/Pages/Create.cshtml.cs
+++ PresentationPlanner/Areas/Contacts/Pages/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PresentationPlanner.Areas.Contacts.Models;
 
@@ -5,8 +6,34 @@ namespace PresentationPlanner.Areas.Contacts.Pages;
 
 public class CreateModel : PageModel
 {
+    [BindProperty]
     public Contact? Contact { get; set; }
+    public IWebHostEnvironment Env { get; }
+
+    public CreateModel(IWebHostEnvironment env)
+    {
+        Env = env;
+    }
+
     public void OnGet()
     {
+    }
+
+    public async Task<IActionResult> OnPostAsync(CancellationToken cancellationToken)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        Contact.Id = Guid.NewGuid();
+        Contact.PhotoUrl = Path.Combine("Images", "Contacts", $"{Contact.Id}-{Contact.Picture.FileName}");
+        var fullpath = Path.Combine(Env.WebRootPath, Contact.PhotoUrl);
+        using (var fileStream = new FileStream(fullpath, FileMode.Create))
+        {
+            await Contact.Picture.CopyToAsync(fileStream, cancellationToken);
+        }
+
+        return RedirectToPage();
     }
 }
